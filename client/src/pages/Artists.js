c8a05c3da7359f297a07b83eef70219451863c29
Kyle <khartson2017@gmail.com>
2022-12-13 23:40:43 -0500

    added model pagination for artists, styled artist view page

--- client/src/pages/Artists.js
+++ client/src/pages/Artists.js
@@ -1,33 +1,70 @@
 import React, { useState, useEffect } from 'react';
 import ArtistPreview from '../components/ArtistPreview';
-import { Button } from 'react-bootstrap'; 
+import { Button, Container, Row, Col } from 'react-bootstrap'; 
 
 function Artists() {
   const [artists, setArtists] = useState([]);
 
+  const [urls, setUrls] = useState({
+    next: null,
+    last: null, 
+    page: null,
+  })
+
+  const [endReached, setEndReached] = useState(false);
+
   useEffect(()=>{
     fetch('/artists').then((r)=>{
       if (r.ok){
-        r.json().then((artists)=>setArtists(artists));
+        r.json().then((r)=>{
+          setArtists(r.data);
+          setUrls({ 
+            next: r.next_url,
+            last: r.last_url,
+            page: r.page_url
+          })
+        })
+      
       }
     })
   }, []);
 
   function showMoreArtists() {
-    console.log('clicked');
+    if (urls.page === urls.last) {
+      setEndReached(true);
+    } else {
+      fetch(`${urls.next}`).then((r)=>{
+        if(r.ok) {
+          r.json().then((r)=>{
+            setArtists([...artists, ...r.data]);
+            setUrls({...urls, page: r.page_url, next: r.next_url});
+          })
+        }
+      })
+    }
   }
+
   return(
-    <>
+    <Container>
+      <Row sm={'auto'}>
       { artists.length === 0 ? (
           <div>empty</div>
       ) : (
-          <>{artists.map((artist)=>{
-              return <ArtistPreview artist={artist}/>
+        <>
+          {artists.map((artist)=>{
+              return (
+                <Col>
+                  <ArtistPreview artist={artist}/>
+                </Col>
+              )
             })}
-            <Button onClick={showMoreArtists}>Show More</Button>
-          </>
+        </>
       )}
-    </>
+      </Row>
+      <Row>
+            <Button disabled={endReached} onClick={showMoreArtists}>Show More</Button>
+      </Row>
+    </Container>
   )
 } 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+kyle = User.create!({ username: "khartson2017", password: 'hello', 
+                     profile_attributes: {
+                      name: "Kyle",
+                      bio: 'Fall of Troy superfan', 
+                      pfp_url: 'https://external-preview.redd.it/A9P8zspGcFahsizLp_5bB5Zk6gHUmU2rlkO8buxpjdg.jpg?auto=webp&s=6d7728a892b380d052b794504eadd2784f0b84d1'
+                    }
+                  })
+artists = Artist.create([
+  { name: "The Fall of Troy", location: "WAS, US", image_url: 'https://media.altpress.com/uploads/2020/08/FALL-OF-TROY.jpg'},
+  { name: "Agalloch", location: "WAS, US", image_url: "https://upload.wikimedia.org/wikipedia/en/1/13/The_Mantle.jpg"},
+  { name: "Dance Gavin Dance", location: "CA, US", image_url:"https://www.pvconcerthall.com/assets/img/dancegavindance_eventimage-1580ea2ae4.jpg"},
+  { name: "Streetlight Manifesto", location: "NJ, US", image_url: "https://s3.amazonaws.com/bit-photos/large/11965309.jpeg"},
+  { name: "Peter Cat Recording Company", location: "Delhi, India", image_url: "https://sungenre.com/wp-content/uploads/2019/06/peter-cat-recording-co-bismillah.jpg"},
+  { name: "Pierce the Veil", location: "CA, US", image_url: "https://s1.ticketm.net/dam/a/341/3fcb5538-bae3-4be3-b94c-1a3404de7341_263031_TABLET_LANDSCAPE_LARGE_16_9.jpg"},
+  { name: "Between the Buried and Me", location: "NC, US", image_url: "https://i.scdn.co/image/ab6761610000e5eb87fc314a9b6b9f18e6e32278"},
+])
