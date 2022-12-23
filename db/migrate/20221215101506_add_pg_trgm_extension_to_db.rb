61efb7e7d32bf7a464d8516cf145bebfde0d74e1
Kyle <khartson2017@gmail.com>
2022-12-23 13:21:08 -0500

    disabled line adding pg_trgm

--- db/migrate/20221215101506_add_pg_trgm_extension_to_db.rb
+++ db/migrate/20221215101506_add_pg_trgm_extension_to_db.rb
@@ -1,6 +1,6 @@
 class AddPgTrgmExtensionToDb < ActiveRecord::Migration[6.1]
   def change
-    execute "create extension if not exists pg_trgm;"
+    # execute "create extension if not exists pg_trgm;"
     # enable_extension "pg_trgm"
   end
 end
